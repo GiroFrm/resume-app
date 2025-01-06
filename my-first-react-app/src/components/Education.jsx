362132d4344c2c9e14bf3a2fa61a9cefba93ff0e
GiroFrm <girofram844@gmail.com>
2025-01-06 20:20:43 +1100

    Refactored Education component to streamline the onSubmit event handling.
    - Removed handleSubmit function and directly used onAddEducation as the form's onSubmit handler.
    - Simplified the component structure for better readability and maintainability.

--- my-first-react-app/src/components/Education.jsx
+++ my-first-react-app/src/components/Education.jsx
@@ -1,40 +1,20 @@
-import { useState } from 'react'
+import { useState } from 'react';
+
 export function Education({ onAddEducation}){
  
-    function handleSubmit(event){
-        event.preventDefault(); 
-
-        const schoolNameInput = event.target['input-school'].value; 
-        const titleInput = event.target['input-title'].value; 
-        const startStudyDate = event.target['input-startdate'].value; 
-        const endStudyDate = event.target['input-enddate'].value; 
-
-        const newEducationInfo = {
-            id: schoolNameInput + startStudyDate, 
-            schoolName: schoolNameInput, 
-            title: titleInput, 
-            startStudyDate: startStudyDate, 
-            endStudyDate: endStudyDate 
-        };
-         onAddEducation(newEducationInfo);
-        // Clear form fields 
-         event.target.reset(); 
-    }
-
-
-
+   
     return(
         <div className='education'>
        <h3>Education</h3>
-       <form onSubmit={handleSubmit}>
+       <form onSubmit={onAddEducation}>
        <p>School Name:</p>
-       <input type="text" name="input-school"  required/>
+       <input type="text" name="input-school" required/>
        <p>Title of Study:</p>
        <input type="text" name="input-title"  required/>
        <p>Begining Date:</p>
-       <input type="date" name="input-startdate"  required/>
+       <input type="date" name="input-startdate"   required/>
        <p>End Date:</p>
-       <input type="date" name="input-enddate"   required/>
+       <input type="date" name="input-enddate"    required/>
        <button type="submit">Add</button>
        </form>
        </div>
