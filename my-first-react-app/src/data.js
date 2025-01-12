ec7d9950d7812f1dbbf8ee3cb0f97ee459d796cf
GiroFrm <girofram844@gmail.com>
2025-01-12 17:04:35 +1100

    Add initial data structures for companies and education
    - Added companies array with sample data of company details including id, companyName, jobTitle, startWorkDate, endWorkDate, and flag.
    - Added education array with sample data of education details including id, schoolName, title, startStudyDate, endStudyDate, and flag.
    - Added newCompany object template for creating new company entries.
    - Added newEducation object template for creating new education entries.
    
    These data structures will be used for managing and displaying company and education information.

---
+++ my-first-react-app/src/data.js
@@ -0,0 +1,56 @@
+export const  companies=[
+{
+    id:0,
+    companyName: "Microsoft",
+    jobTitle:"Software Engineer",
+    startWorkDate:"2012-03-06",
+    endWorkDate:"2014-04-14",
+    flag:false
+  },
+  {
+    id:1,
+    companyName: "Google",
+    jobTitle:"Software Engineer",
+    startWorkDate:"2014-05-14",
+    endWorkDate:"2016-04-19",
+    flag:false
+  }
+];
+
+export const education = [
+     {
+        id:0,
+        schoolName: "University of Melbourne",
+        title: "phd in Computer Science",
+        startStudyDate:"2003-01-05",
+        endStudyDate:"2006-04-07",
+        flag:false
+       },
+       {
+        id:1,
+        schoolName: "University of Milano",
+        title: "phd in phisics",
+        startStudyDate:"2008-04-09",
+        endStudyDate:"2011-05-10",
+        flag:false
+       }
+];
+
+
+export const newCompany = {
+    id:'',
+    companyName: "",
+    jobTitle: "",
+    startWorkDate:"",
+    endWorkDate:"",
+    flag:''   
+}
+
+export const newEducation = {
+    id:'',
+    schoolName: "",
+    title: "",
+    startStudyDate:"",
+    endStudyDate:"",
+    flag:''
+}
